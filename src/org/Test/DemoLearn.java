291cbe7c33fe6238437f7aa6bbd949ef8b8b4170
Naveenchander12 <naveenchander12@gmail.com>
2023-06-27 14:22:14 +0530

    mauli code

--- src/org/Test/DemoLearn.java
+++ src/org/Test/DemoLearn.java
@@ -5,5 +5,9 @@ public class DemoLearn {
 	public static void main(String[] args) {
 		System.out.println("Demo Test Learning");
 	}
+	
+	public void m1 () {
+		System.out.println("hi Moulisevaran");
+	}
 
 }
